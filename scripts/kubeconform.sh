ddfa2f43c99834531fadec2e82c31fe5783a0328
brunnels <kraven@kraven.org>
2024-07-22 16:35:59 -0500

    trying to fix kubeconform

--- scripts/kubeconform.sh
+++ scripts/kubeconform.sh
@@ -12,7 +12,7 @@ kubeconform_args=(
     "-strict"
     "-ignore-missing-schemas"
     "-skip"
-    "Secret,ReplicationSource"
+    "Secret"
     "-schema-location"
     "default"
     "-schema-location"
