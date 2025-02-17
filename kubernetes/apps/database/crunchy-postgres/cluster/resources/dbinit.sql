4acecc6894e51f4c50f1a73c766e4c50dfb65193
brunnels <kraven@kraven.org>
2025-02-17 17:12:16 +0000

    add dashbrr db

--- kubernetes/apps/database/crunchy-postgres/cluster/resources/dbinit.sql
+++ kubernetes/apps/database/crunchy-postgres/cluster/resources/dbinit.sql
@@ -6,7 +6,5 @@ CREATE DATABASE dashbrr;
 \c dashbrr;
 
 -- Grant all privileges to the dashbrr user
-GRANT ALL PRIVILEGES ON DATABASE dashbrr TO dashbrr;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO dashbrr;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO dashbrr;
+GRANT ALL ON SCHEMA public TO dashbrr;
 ---------- END DASHBRR --------------
