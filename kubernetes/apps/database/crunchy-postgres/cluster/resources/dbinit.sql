0960e2bb5a9eaa356b6faf5af6f71f0c663d140e
brunnels <kraven@kraven.org>
2025-02-17 15:44:16 +0000

    add dbinit for dashbrr

--- kubernetes/apps/database/crunchy-postgres/cluster/resources/dbinit.sql
+++ kubernetes/apps/database/crunchy-postgres/cluster/resources/dbinit.sql
@@ -5,25 +5,6 @@ CREATE DATABASE dashbrr;
 -- Connect to the test database
 \c dashbrr;
 
--- Create the necessary tables
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    username TEXT UNIQUE NOT NULL,
-    email TEXT UNIQUE NOT NULL,
-    password_hash TEXT NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS service_configurations (
-    id SERIAL PRIMARY KEY,
-    instance_id TEXT UNIQUE NOT NULL,
-    display_name TEXT NOT NULL,
-    url TEXT,
-    api_key TEXT,
-    access_url TEXT
-);
-
 -- Grant all privileges to the dashbrr user
 GRANT ALL PRIVILEGES ON DATABASE dashbrr TO dashbrr;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO dashbrr;
